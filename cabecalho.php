57b00446e1c19f439051be91aede43ba194753de
VitorZF <vitor.pereira@fabricadecodigos.com.br>
2021-04-12 21:19:43 -0300

    alterado titulo do modal

--- cabecalho.php
+++ cabecalho.php
@@ -28,7 +28,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="tituloModalLoginCadastro">Login de colaborador</h5>
+                <h5 class="modal-title" id="tituloModalLoginCadastro">Login de colaboradores</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
